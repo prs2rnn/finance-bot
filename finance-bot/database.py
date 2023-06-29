c4e95d8b50e8b9424a84dc56ba9d294efbce3189
luculliano <vederecento@gmail.com>
2023-06-30 00:02:19 +0500

    restructure database

--- finance-bot/database.py
+++ finance-bot/database.py
@@ -11,32 +11,36 @@ class Request:
 
     async def get_categories(self) -> str:
         res = await self.connector.fetch("select codename, aliases from expense_category")
-        expenses = "\n".join(map(lambda x: f"{x['codename']} ({x['aliases']})", res))
+        expenses = "\n".join(map(lambda x: f"• {x['codename']} ({x['aliases']})", res))
         res = await self.connector.fetch("select * from income_category")
-        incomes = "\n".join(map(lambda x: f"{x['codename']} ({x['aliases']})", res))
+        incomes = "\n".join(map(lambda x: f"• {x['codename']} ({x['aliases']})", res))
         return f"{expenses}\n\n{incomes}"
 
     async def get_last_changes(self) -> str:
         res = await self.connector.fetch("select expense_id as id, amount, codename, "
-                          "created, table_name from (select *, 'expense' as table_name "
-                          "from expense union select *, 'income' as table_name from "
-                          "income order by created desc limit 5) as a order by created asc")
+                        "created, table_name from (select *, 'expense' as table_name "
+                        "from expense union select *, 'income' as table_name from "
+                        "income order by created desc limit 5) as a order by created asc")
         changes = "\n".join(map(lambda x: f"{res.index(x) + 1}) "
-                                f"{round(x['amount'], 2)} rub {x['codename']} "
-            f"({datetime.strftime(x['created'], '%d.%m.%Y %H:%M:%S')}) "
-            f"/del_{x['table_name']}_{x['id']}", res))
+                                f"{round(x['amount'], 1)} rub, {x['codename']}, "
+            f"{datetime.strftime(x['created'], '%d-%m-%y')}\n"
+            f"(/del_{x['table_name']}_{x['id']})", res))
         return changes
 
     async def delete_change(self, table_name: str, id_: int) -> int:
         res = await self.connector.execute(f"delete from {table_name} where "
-                                     f"{table_name}_id = $1", id_)
+                                           f"{table_name}_id = $1", id_)
         return int(res.split()[1])
+
+    async def add_record(self, amount: float, category: str, raw_text: str) -> None:
+        table_name = ("expense", "income")[amount > 0]
+        await self.connector.execute(f"insert into {table_name} ()")
 
 
 async def main() -> None:
     """Temporary function to test methods"""
     async with asyncpg.create_pool(DSN) as pool:
-        print(await Request(pool).delete_change("expense", 6))
+        print(await Request(pool).get_categories())
 
 
 if __name__ == "__main__":
--- init.sh
+++ init.sh
@@ -8,12 +8,12 @@ grant all on database ${DB_NAME} to ${DB_USER};
 \c ${DB_NAME}
 grant all on schema public to ${DB_USER};
 \c ${DB_NAME} ${DB_USER}
-create table if not exists expense_category(
+create table if not exists category(
     codename varchar(50) primary key,
-    is_base_expense boolean,
-    aliases text
+    aliases text,
+    is_expense boolean
 );
-create table if not exists expense(
+create table if not exists record(
     expense_id integer primary key generated always as identity,
     amount numeric(13, 3),
     created timestamptz(3),
@@ -21,34 +21,23 @@ create table if not exists expense(
     raw_text text,
     FOREIGN KEY(codename) REFERENCES expense_category(codename)
 );
-create table if not exists income_category(
-    codename varchar(50) primary key,
-    aliases text
-);
-create table if not exists income(
-    income_id integer primary key generated always as identity,
-    amount numeric(13, 2),
-    created timestamptz(3),
-    codename varchar(50),
-    raw_text text,
-    FOREIGN KEY(codename) REFERENCES income_category(codename)
-);
-insert into expense_category(codename, is_base_expense, aliases)
+insert into category(codename, aliases, is_expense)
 values
-    ('food', true, 'food,meal,еда,продукты'),
-    ('transport', true, 'bus,транспорт'),
-    ('pharmacy', true, 'аптека,лекарства'),
-    ('mobile', true, 'связь,phone,телефон'),
-    ('internet', true, 'инет,inet'),
-    ('barber', true, 'hair,стрижка,барбер'),
-    ('lunch', false, 'ланч,столовая,ресторан,кафе,cafe'),
-    ('cab', false, 'такси,taxi,cab'),
-    ('relax', false, 'прогулка,свидание'),
-    ('clothes', false, 'шмот,одежда'),
-    ('gift', false, 'подарки'),
-    ('car', false, 'машина,авто,auto'),
-    ('subscription', false, 'подписка,sub'),
-    ('other',  true, 'прочее,другое');
-insert into income_category(codename, aliases) values
-    ('salary', 'зп'), ('business', 'dividends,extra'), ('gift', 'from other');
+    ('food', 'food,meal,еда,продукты', true),
+    ('transport', 'bus,транспорт', true),
+    ('pharmacy', 'аптека,лекарства', true),
+    ('mobile', 'связь,phone,телефон', true),
+    ('internet', 'инет,inet', true),
+    ('barber', 'hair,стрижка,барбер', true),
+    ('lunch', 'ланч,столовая,ресторан,кафе,cafe', true),
+    ('cab', 'такси,taxi,cab', true),
+    ('relax', 'прогулка,свидание', true),
+    ('clothes', 'шмот,одежда', true),
+    ('gift', 'подарки', true),
+    ('car', 'машина,авто,auto', true),
+    ('subscription', 'подписка,sub', true),
+    ('other', 'прочее,другое', true);
+    ('salary', 'зп', false),
+    ('business', 'dividends,extra', false),
+    ('gift', 'from other', false);
 EOF
