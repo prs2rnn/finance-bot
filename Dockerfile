e0b721cea535be8b152c1a97278fb0f4af1570b0
luculliano <vederecento@gmail.com>
2023-07-02 23:25:14 +0500

    add timezone and date

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM python:3.11.3-alpine3.18
 
+ENV TZ Europe/Moscow
+
 ENV \
     PYTHONDONTWRITEBYTECODE=1 \
     PYTHONUNBUFFERED=1 \
--- finance-bot/vocabulary.py
+++ finance-bot/vocabulary.py
@@ -1,6 +1,10 @@
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
 VOCABULARY = {
-    "notify_week": "<b>Weekly backup and statistics here</b>\n\nExpenses: "
-                   "{expenses}₽\nIncomes\t{incomes}₽\n"
+    "notify_week": f"<b>{datetime.now(ZoneInfo('Europe/Moscow')).strftime('%W')} "
+                   "week backup and statistics "
+                   "here</b>\n\nExpenses: {expenses}₽\nIncomes\t{incomes}₽\n"
                    "Savings\t{savings}₽ from planned {plan_savings}₽",
     "backup_error": "Backup file is more than 20MB",
     "start": "<b>Bot for financial accounting</b>\n\n"
@@ -18,8 +22,8 @@ VOCABULARY = {
     "delete_true": "<b>Delete record</b>\n\nAdd record: 250 cab\nLast records: /records\n"
                    "Current month: /month",
     "other": "<b>Not recognized</b>\n\nTo add record: 250 cab\nOr use /help for help",
-    "month": "<b>Month statistics</b>\n\nExpenses\t{expenses}₽"
-             "\nIncomes\t{incomes}₽\n"
+    "month": f"<b>Month statistics for {datetime.now(ZoneInfo('Europe/Moscow')).strftime('%B')}</b>\n\n"
+             "Expenses\t{expenses}₽\nIncomes\t{incomes}₽\n"
              "Savings\t{savings}₽ from planned {plan_savings}₽"
              "\n\nAdd record: 250 bus\nCategories: /categories\nLast records: /records",
     "records": "{content}\n\nAdd record: 250 cab\nCategories: /categories"
@@ -35,24 +39,23 @@ VOCABULARY = {
     "db_categories": "<b>List of expense category</b>\n{expenses}\n\n<b>"
                      "List of income category</b>\n{incomes}",
     "db_category": "• {codename} ({aliases})",
-    "year": "",
 }
 
 SQL_STATISTICS_CUR = """
 select expenses, incomes, savings, incomes * 0.15 as planned_savings from
-(select to_char(created, '{period}') as {period}_, sum(amount) as savings from record r
+(select extract({period} from created) as {period}_, sum(amount) as savings from record r
 	join category c on r.codename = c.codename
 	where created >= date_trunc('{period}', now()) and r.codename = 'savings'
 	group by {period}_) as savings
 full join
-(select to_char(created, '{period}') as {period}_, sum(amount) as incomes from record r
+(select extract({period} from created) as {period}_, sum(amount) as incomes from record r
 	join category c on r.codename = c.codename
 	where created >= date_trunc('{period}', now())
 	and is_expense = false
 	group by {period}_) as planned_savings
 using({period}_)
 full join
-(select to_char(created, '{period}') as {period}_, sum(amount) as expenses from record r
+(select extract({period} from created) as {period}_, sum(amount) as expenses from record r
 	join category c on r.codename = c.codename
 	where created >= date_trunc('{period}', now()) and is_expense = true
 	and r.codename <> 'savings'
@@ -62,7 +65,7 @@ using({period}_);
 
 
 SQL_STATISTICS_PREV = """
-select expenses, incomes, savings, incomes * 0.15 as planned_savings from
+select incomes, savings, incomes * 0.15 as planned_savings from
 (select extract({period} from created) as {period}_, sum(amount) as savings from record r
 	join category c on r.codename = c.codename
 	where created >= date_trunc('{period}', now()) - interval '1 {period}'
